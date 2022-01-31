c01766c37a463f10b0c7354226e73f9118d3097a
Lucas Moraes <lucasmoraes2@outlook.com.br>
2022-01-31 19:30:11 -0300

    Add factory method design pattern

---
+++ src/creational/factory-method/client-code.ts
@@ -0,0 +1,19 @@
+import { CarFactory } from './factories/car-factory';
+import { randomVehicleAlgorithm } from './main/random-vehicle-algorithm';
+import { randomNumbers } from './utils/random-numbers';
+
+// const carFactory = new CarFactory();
+const customerNames = ['Phoebe', 'Joey', 'Aurora', 'Florence'];
+
+for (let i = 0; i < 10; i++) {
+    const vehicle = randomVehicleAlgorithm();
+    const name = customerNames[randomNumbers(customerNames.length)];
+    vehicle.pickUp(name);
+    vehicle.stop();
+    // const newCar = carFactory.pickUp(
+    //     name,
+    //     `NOVO CARRO - ${randomNumbers(100)}`,
+    // );
+    // newCar.stop();
+    console.log('---');
+}
